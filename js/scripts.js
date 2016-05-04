6f055f25c8eac95ea093c3e5db8a2f9adc102c02
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-05-03 21:19:36 -0700

    create accounts and transactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,84 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 // business logic
-function createAccount(first, last, init) {
+function Account(first, last, init) {
+  this.firstName=first;
+  this.lastName=last;
+  this.balance=parseFloat(init);
+}
 
+Account.prototype.transaction=function(amount,type) {
+  if(type===1) {
+    this.balance+=amount;
+  }
+  if(type===2) {
+    this.balance-=amount;
+  }
 }
 
 // user interface logic
+var accountExists=false;
+
 $("#newAccount").click(function(event) {
   event.preventDefault();
-  alert($("#firstname").val());
-  alert($("#lastname").val());
-  alert($("#initialdeposit").val());
-  createAccount($("#firstname").val(), $("#lastname").val(), $("#initialdeposit").val());
+  newAccount= new Account($("#firstname").val(), $("#lastname").val(), $("#initialdeposit").val());
+  accountExists=true;
+  $("#openAccount").hide();
+  $("#transactionRecord").append("<li>$"+ newAccount.balance.toFixed(2) + " - Initial Deposit </li>");
+  $("#currentBalance").text("$" + newAccount.balance.toFixed(2));
+  $("#showBalance").show();
 });
 
+$("#submitTransaction").click(function(event) {
+  event.preventDefault();
+  var transactionAmount=parseFloat($("#transactionAmount").val());
+  var transactionType=0;
+  if(parseInt($("input:checked").val())===1) {
+    var transactionType=1;
+    $("#transactionRecord").append("<li>$"+ transactionAmount.toFixed(2) + " - " + $("#transactionNote").val() + "</li>");
+    newAccount.transaction(transactionAmount, transactionType);
+    $("#transactions").hide();
+    $("#currentBalance").text("$" + newAccount.balance.toFixed(2));
+    $("#showBalance").show();
+  } else if (parseInt($("input:checked").val())===2) {
+    var transactionType=2;
+    $("#transactionRecord").append("<li>($"+ transactionAmount.toFixed(2) + ") - " + $("#transactionNote").val() + "</li>");
+    newAccount.transaction(transactionAmount, transactionType);
+    $("#transactions").hide();
+    $("#currentBalance").text("$" + newAccount.balance.toFixed(2));
+    $("#showBalance").show();
+  } else {
+    alert("invalid transaction type, please select deposit or withdrawal");
+  }
+
+});
 
 $("#open").click(function() {
-  $("#openAccount").show();
-  $("#transactions").hide();
-  $("#showBalance").hide();
+  if(accountExists) {
+    alert("You already have an account with the bank, please use the transaction tab for deposits!")
+  } else {
+    $("#openAccount").show();
+    $("#transactions").hide();
+    $("#showBalance").hide();
+  }
+
 });
 $("#change").click(function() {
-  $("#openAccount").hide();
-  $("#transactions").show();
-  $("#showBalance").hide();
+  if(accountExists) {
+    $("#openAccount").hide();
+    $("#transactions").show();
+    $("#showBalance").hide();
+  } else {
+    alert("You cannot make a transaction because you do not have an account with this bank. Please open an account!");
+  }
+
 });
 $("#view").click(function () {
-  $("#openAccount").hide();
-  $("#transactions").hide();
-  $("#showBalance").show();
+  if(accountExists) {
+    $("#openAccount").hide();
+    $("#transactions").hide();
+    $("#showBalance").show();
+  } else {
+    alert("We cannot show you your balance because you do not have an account with this bank. Please open an account!")
+  }
+
 });
